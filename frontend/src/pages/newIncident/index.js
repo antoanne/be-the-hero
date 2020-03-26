903926f8b51bd38b0110590746df19832fbdd081
Antoanne Pontes <antoanne@gmail.com>
2020-03-26 20:11:56 -0300

    Páginas estáticas com estilo no frontend

---
+++ frontend/src/pages/newIncident/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+
+export default function NewIncident() {
+    return (
+        <div className="new-incident-container">
+            <div className="content">
+                <section>
+                    <Link to="/">
+                        <img src={logoImg} alt="be The Hero"/>
+                    </Link>
+                    <h1>Cadastrar novo caso</h1>
+                    <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+
+                    <Link className="back-link" to="/profile">
+                        <FiArrowLeft size={16} color="#E02041" />
+                        Voltar para home
+                    </Link>
+                </section>
+
+                <form>
+                    <input placeholder="Título do caso" />
+                    <textarea placeholder="Descrição" />    
+                    <input placeholder="Valor em R$" />    
+
+                    <div className="button-group">
+                        <button className="button button-cancel" type="submit">Cancelar</button>
+                        <button className="button" type="submit">Cadastrar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
