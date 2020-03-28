722ebec6498c7f29544940d232a0a84e112a1256
Antoanne Pontes <antoanne@gmail.com>
2020-03-28 20:06:33 -0300

    Front-end do app web

--- frontend/src/pages/logon/index.js
+++ frontend/src/pages/logon/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -8,25 +10,51 @@ import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
-  return (
-    <div className="logon-container">
-        <section className="form">
-            <Link to="/">
-                <img src={logoImg} alt="be The Hero"/>
-            </Link>
+    const history = useHistory();
+    const [id, setId] = useState('');
+    
+    async function handleLogin(e) {
+        e.preventDefault();
 
-            <form>
-                <h1>Faça seu Logon</h1>
-                <input placeholder="Sua ID"/>
-                <button type="submit" className="button">Entrar</button>
-                
-                <Link className="back-link" to="/register">
-                    <FiLogIn size={16} color="#E02041" />
-                    Não tenho cadastro
+        const data = {
+            id
+        };
+
+        try {
+            const response = await api.post('sessions', data);
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+            
+            history.push('/profile');
+        } catch (error) {
+            alert('Acesso inválido, tente novamente.');
+        }
+        
+    }
+
+
+    return (
+        <div className="logon-container">
+            <section className="form">
+                <Link to="/">
+                    <img src={logoImg} alt="be The Hero"/>
                 </Link>
-            </form>
-        </section>
-        <img src={heroesImg} alt='Heroes' />
-    </div>
-  );
+
+                <form onSubmit={handleLogin}>
+                    <h1>Faça seu Logon</h1>
+                    <input placeholder="Sua ID"
+                        value={id}
+                        onChange={ e => setId(e.target.value)} />
+                    <button type="submit" className="button">Entrar</button>
+                    
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </Link>
+                </form>
+            </section>
+            <img src={heroesImg} alt='Heroes' />
+        </div>
+    );
 }
--- frontend/src/pages/newIncident/index.js
+++ frontend/src/pages/newIncident/index.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import './styles.css';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 export default function NewIncident() {
+    const history = useHistory();
+
+    const ongId = localStorage.getItem('ongId');
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+
+    if (!ongId) handleLogout();
+
+    function handleLogout() {
+        localStorage.clear();
+        history.push('/');
+    }
+    
+    async function handleNewIncident(e) {
+        e.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value
+        };
+
+        try {
+            await api.post('incidents', data, {
+                headers: {
+                    authorization: ongId,
+                }
+            });
+            
+            history.push('/profile');
+        } catch (error) {
+            alert('Erro ao cadastrar novo caso!');
+        }
+        
+    }
+
     return (
         <div className="new-incident-container">
             <div className="content">
@@ -22,10 +62,16 @@ export default function NewIncident() {
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholder="Título do caso" />
-                    <textarea placeholder="Descrição" />    
-                    <input placeholder="Valor em R$" />    
+                <form onSubmit={handleNewIncident}>
+                    <input placeholder="Título do caso"
+                        value={title}
+                        onChange={ e => setTitle(e.target.value)} />
+                    <textarea placeholder="Descrição" 
+                        value={description}
+                        onChange={ e => setDescription(e.target.value)} />    
+                    <input placeholder="Valor em R$" 
+                        value={value}
+                        onChange={ e => setValue(e.target.value)} />    
 
                     <div className="button-group">
                         <button className="button button-cancel" type="submit">Cancelar</button>
